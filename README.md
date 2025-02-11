527f144ba56b25b9c4c5ceff9daf3a1ba3a68de0
Vedant Narwade <vedantnarwade121@gmail.com>
2025-02-12 00:59:42 +0530

    add card and search design. Added sanity with author and startup models/Schemas

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
-
+Groq -- sanity query language
 First, run the development server:
 
 ```bash
@@ -19,6 +19,10 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
 This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+
+![Schema Digram](./public/schemadescription.png)
+![Schema Digram](./public/flow.png)
+
 
 ## Learn More
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,44 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default function Home() {
+export default async function Home({ searchParams } : {searchParams: Promise<{ query?: string}>}) {
+
+  const query = (await searchParams).query
+
+  const post = [{
+    _createdAt: new Date(),
+    _id:1,
+    views: 55,
+    author: { _id: 1 , name: "Adrian"},
+    description: "This is a description",
+    image:"https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg",
+    catagory:"Robot",
+    title: "We Robot"
+  },
+]
+
   return (
     <>
-    <div className="heading">Home</div>
+    <section className="blue_container">
+      <h1 className="heading">Pitch Your Startup, <br /> connect with Entrepreneur</h1>
+      <p className="sub-heading !max-w-3xl"> Submit Ideas, Vote on Pitches, and Get Noticed in virtual COmputations </p>
+      <SearchForm query={query} />
+    </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `Search results for "${query}"` : "All Startups"}
+      </p>
+      <ul className="mt-7 card_grid">
+        {post?.length > 0 ? (
+          post.map((post: StartupCardType, index: number) =>(
+            <StartupCard key={post?._id } post={post} />
+          ))
+        ) : (
+          <p className="no-results">No startups found</p>
+        )}
+      </ul>
+    </section>
     </>
   );
 }
